a5df8b79cce60b168d34cdbe3343e6fb6c979576
SORAIAROCHA <soraiarsr2017@gmail.com>
2021-11-29 21:26:31 +0000

    exercicio variaveis

---
+++ variaveis/index.js
@@ -0,0 +1,78 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CODIGO
+
+/* Exercício 1
+ 10
+ 10, 5
+
+Exercício 2
+Haverá erro, pois c não foi declarada. 
+Se tivesse sido declarada seria impresso no console o resultado 10 10 10
+
+
+Exercício 3
+O programa calcula o o valor por hora trabalhada
+ horasTrabalhadas
+valorDiaria
+*/
+
+// EXERCÍCIO DE ESCRITA DE CODIGO
+
+// Exercício 1
+
+//a
+let nome
+//b
+let idade
+
+//c
+console.log(typeof nome)
+console.log(typeof idade)
+
+// d 
+// Não foi atribuido valor, assim o console imprimiu undefined.
+
+//e
+let nome = prompt("Digite seu nome")
+let idade = prompt("Digite seu idade")
+
+//f
+console.log(nome, idade)
+console.log(typeof nome)
+console.log(typeof idade)
+
+// Ambas são do tipo string
+
+//g
+
+console.log("Olá", nome, "você tem", idade, "anos")
+
+
+
+// Exercício 2
+
+const primiraPergunta = "Você é brasileiro?"
+const segundaPergunta = "Esta chovendo?"
+const terceiraPergunta = "O galo vai ser campeão?"
+
+//a
+const resposta1 = prompt(primiraPergunta)
+const resposta2 = prompt(segundaPergunta)
+const resposta3 = prompt(terceiraPergunta)
+
+//b
+console.log(primiraPergunta, resposta1)
+console.log(segundaPergunta, resposta2)
+console.log(terceiraPergunta, resposta3) 
+
+
+// Exercício 3
+
+let a = 10
+let b = 25
+let c
+c = a
+a = b
+b = c
+
+console.log(a)
+console.log(b)
