e91b3bf613084872717869166f671ab09c220423
SORAIAROCHA <soraiarsr2017@gmail.com>
2022-01-03 20:29:42 +0000

    correção projeto-lista-js

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -24,8 +24,7 @@ function calculaAreaRetangulo() {
   const areaRetangulo = alturaRetangulo*larguraRetangulo
   return areaRetangulo
 }
-console.log (calculaAreaRetangulo(3,5))
-console.log (calculaAreaRetangulo (2,6))
+
 
 // EXERCÍCIO 02
 function imprimeIdade() {
@@ -35,8 +34,6 @@ function imprimeIdade() {
   return idade
 
 }
-console.log (imprimeIdade (2020, 1990))
-console.log (imprimeIdade (2021, 1967))
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
@@ -52,11 +49,8 @@ function imprimeInformacoesUsuario() {
   const nome = prompt("digite seu nome")
   const idade = prompt(Number("digite sua idade"))
   const email = prompt("digite seu email")
-  const frase = ´Meu nome é ${nome}, tenho ${idade}, e o meu e-mail é ${email}.´
-  return frase
+  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`) 
 }
-console.log (imprimeInformacoesUsuario)
-//NÃO RESOLVIDO
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
@@ -69,7 +63,6 @@ function imprimeTresCoresFavoritas() {
 }
 imprimeTresCoresFavoritas
 
-//NADA NO CONSOLE, NEM O PROMPT ABRIU
 
 
 // EXERCÍCIO 06
@@ -115,7 +108,7 @@ function retornaUltimoElemento(array) {
   const ultimoElemnto = array[array.length-1]
   return ultimoElemnto
 }
-//DEU ERRADO
+
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
@@ -124,34 +117,43 @@ function trocaPrimeiroEUltimo(array) {
   array[0] = ultimoElemnto
   array[array.length-1] = primeiroElemento
   return array
-//DEU ERRADO
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-const comparacao = string1 == string2
-return comparacao
+  return string1.toLowerCase() === string2.toLowerCase()
 }
-console.log (checaIgualdadeDesconsiderandoCase ("ola","olA" ))
-console.log (checaIgualdadeDesconsiderandoCase ("bananinha","BANANINHA"))
-console.log (checaIgualdadeDesconsiderandoCase ("banana","BANANINHA" ))
-//DEU TUDO FALSE 
+
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+  const anoAtual = Number(prompt("Digite o ano atual"))
+   const anoNascimento = Number(prompt("Digite o ano de nascimento"))
+   const anoEmissao = Number(prompt("Digite o ano de emissão do documento"))
 
+   const idade = anoAtual - anoNascimento
+   const tempoCarteira = anoAtual - anoEmissao
+
+   const cond1 = idade <= 20 && tempoCarteira >= 5
+   const cond2 = idade > 20 && idade <= 50 && tempoCarteira >= 10
+   const cond3 = idade > 50 && tempoCarteira >= 15
+
+   console.log(cond1 || cond2 || cond3)
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
-
+  const cond1 = ano % 400 === 0
+   const cond2 = (ano % 4 === 0) && (ano % 100 !== 0)
+   return cond1 || cond2
 }
-
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+  const maiorDeIdade = prompt("Você tem mais de 18 anos?")
+  const ensinoMedio = prompt("Você possui ensino médio completo?")
+  const disponibilidade = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+
+  console.log(maiorDeIdade === "sim" && ensinoMedio === "sim" && disponibilidade === "sim")
 
 }
