11127692a0a12efc2670b42726466a02646d85e9
SORAIAROCHA <soraiarsr2017@gmail.com>
2022-01-26 19:49:23 +0000

    exercicios apis-labenusers

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import TelaCadastro from "./Components/TelaCadastro"
+import TelaUsuarios from "./Components/TelaUsuarios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    telaAtual: "cadastro"
+  };
+
+  escolheTela = () => {
+    if (this.state.telaAtual === "cadastro") {
+      this.setState({ telaAtual: "usuarios" });
+    } else {
+      this.setState({ telaAtual: "cadastro" });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.changePage}>Trocar de tela</button>
+        {this.state.telaAtual === "cadastro" ? <TelaCadastro /> : <users />}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ modulo2/apis-labenusers/apis-labenusers/src/Components/TelaCadastro.js
@@ -0,0 +1,72 @@
+import React from "react";
+import axios from "axios";
+
+class TelaCadastro extends React.Component {
+    state = {
+      name: "",
+      email: ""
+    };
+  
+    handleName = event => {
+      const newNameValue = event.target.value;
+  
+      this.setState({ name: newNameValue });
+    };
+  
+    handleEmail = event => {
+      const newEmailValue = event.target.value;
+  
+      this.setState({ email: newEmailValue });
+    };
+  
+    handleCreateUser = () => {
+      const axiosConfig = {
+        headers: {
+          Authorization: "soraia-rocha-vaughan"
+        }
+      };
+  
+      const body = {
+        name: this.state.name,
+        email: this.state.email
+      };
+  
+      axios
+        .post(
+          "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+          body,
+          axiosConfig
+        )
+        .then(() => {
+          alert(`Usuário ${this.state.name} criado com sucesso!`);
+          this.setState({ name: "", email: "" });
+        })
+        .catch(error => {
+          alert("Erro ao criar o usuário");
+          console.log(error);
+        });
+    };
+  
+    render() {
+      return (
+        <div>
+          <input
+            placeholder="Nome"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
+          <input
+            placeholder="E-mail"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+          <button onClick={this.handleCreateUser}>Criar Usuário</button>
+        </div>
+      );
+    }
+  }
+  
+  export default TelaCadastro;
+  
