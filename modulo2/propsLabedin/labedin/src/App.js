204502d99b44e124b4d66a038f9956329cfe9d79
SORAIAROCHA <soraiarsr2017@gmail.com>
2022-02-14 18:27:09 +0000

    .

--- modulo2/propsLabedin/labedin/src/App.js
+++ modulo2/propsLabedin/labedin/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
-import foto.jpeg from "./foto.jpeg"
-import balanca.png from "./balanca.png"
-import CardPequrno from "./components/CardPequeno/CardPequeno"
+import foto from "./foto.jpeg"
+import balanca from "./balanca.png"
+import CardPequrno from "./components/CardPequeno/CardPequeno.js"
 
 function App() {
   return (
@@ -16,7 +16,6 @@ function App() {
           nome="Soraia" 
           descricao="Oi, meu nome é Soraia. Sou advogada e dev nas horas vagas."
         />
-        
         <ImagemButton 
           imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
           texto="Ver mais"
@@ -26,32 +25,31 @@ function App() {
       <div className="page-section-container">
         <h2>Experiências profissionais</h2>
         <CardGrande 
-          imagem="/home/soraia/LABENU/vaughan-Soraia-Rocha/modulo2/propsLabedin/labedin/src/balanca.png" 
+          imagem= {foto} 
           nome="Advogada" 
           descricao="Resolver tretas!" 
         />
-    
       </div>
 
       <div className="page-section-container">
         <h2>Minhas redes sociais</h2>
         <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          imagem={balanca} 
           texto="Facebook" 
         />        
-
         <ImagemButton 
           imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
           texto="Twitter" 
         />        
       </div>
       
-      <div className="cardPequeno-container">
+      <div className="CardPequeno-container">
         <h2>Contatos</h2>
-        <ImagemButton 
+        <CardPequrno 
           email="soraia@gmail.com" 
           telefone="123456" 
-        />      
+        />     
+        </div> 
     </div>
   );
 }
