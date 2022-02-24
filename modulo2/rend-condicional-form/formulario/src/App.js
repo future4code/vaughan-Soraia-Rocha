1172df58758e6e9a8feb61eca190514a9cf5cef8
SORAIAROCHA <soraiarsr2017@gmail.com>
2022-02-24 20:43:24 +0000

    exercicio renderização condicional

--- modulo2/rend-condicional-form/formulario/src/App.js
+++ modulo2/rend-condicional-form/formulario/src/App.js
@@ -1,12 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Etapa1 from "./components/Etapa1";
+import Etapa2 from "./components/Etapa2";
+import Etapa3 from "./components/Etapa3";
+import Final from "./components/Final";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const AppContainer = styled.div`
+{
+  font-family: sans-serif;
+  text-align: center;
+} `
+
+class App extends React.Component {
+  state = {
+      etapa: 1
+  };
+
+  renderizaEtapa = () => {
+    switch (this.state.etapa) {
+      case 1:
+        return <Etapa1 />;
+      case 2:
+        return <Etapa2 />;
+      case 3:
+        return <Etapa3 />;
+      case 4:
+        return <Final />;
+      default:
+        return <Final />;
+    }
+  };
+
+  proximaEtapa = () => {
+    this.setState({ etapa: this.state.etapa + 1 });
+  };
+
+  render() {
+    return (
+      <AppContainer>
+        {this.renderizaEtapa()}
+        <br />
+        {this.state.etapa !== 4 && (
+          <button onClick={this.proximaEtapa}>Próxima etapa</button>
+        )}
+      </AppContainer>
+    );
+  }
 }
 
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
--- modulo2/rend-condicional-form/formulario/src/components/Etapa1.js
+++ modulo2/rend-condicional-form/formulario/src/components/Etapa1.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
+import React from "react"
+import PerguntaAberta from "./PerguntaAberta"
+import PerguntaOpcoes from "./PerguntaOpcoes"
+
+class Etapa1 extends React.Component {
+  render() {
+    return (
+      <div>
+
+         <h3>ETAPA 1 - DADOS GERAIS</h3>
+
+          <PerguntaAberta pergunta={"1. Qual o seu nome?"} />
+          <PerguntaAberta pergunta={"2. Qual sua idade?"} />
+          <PerguntaAberta pergunta={"3. Qual seu email?"} />
+          <PerguntaOpcoes
+          pergunta={"4. Qual a sua escolaridade?"}
+          opcoes={[
+            "Ensino médio incompleto",
+            "Ensino médio completo",
+            "Ensino superior incompleto",
+            "Ensino superior completo"
+          ]}
+      />
     </div>
   );
 }
-
-export default App;
+}
+export default Etapa1;
